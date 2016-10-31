39199792423276795b06a92cfd91be8cc5f5d6ea
Basile Vu <basile.vu@gmail.com>
2016-10-31 11:48:18 +0100

    Fixing a bug when 0 batonnets

--- batonnet.js
+++ batonnet.js
@@ -1,6 +1,7 @@
 Batonnet = function () {
 	console.log("Processing Batonnets")
 	let c = '|'
+	let nBatonnets = 5
 	let batonnets = document.querySelectorAll('.batonnet:not(.batonnet-processed)')
 	;[].forEach.call(batonnets, function (batonnet) {
 		process(batonnet)
@@ -8,22 +9,23 @@ Batonnet = function () {
 
 	function process(batonnet) {
 		let value = parseInt(batonnet.innerText)
-		let nbGroup = Math.ceil(value / 5)
+		let nbGroup = Math.ceil(value / nBatonnets)
 
 		var groupsResult = []
 
 		if (value === 0) {
 			batonnet.innerHTML = '&ndash;'
+			batonnet.classList.add('batonnet-processed')
 			return
 		}
 
 		for (let i = 0; i < nbGroup; i++) {
-			if (i < nbGroup - 1 || value % 5 === 0) {
+			if (i < nbGroup - 1 || value % nBatonnets === 0) {
 				groupsResult.push('<span class="batonnet-group">' + c + c + c + c + '<span class="batonnet-slash">' + c + '</span></span>')
 			} else {
 				var lastGroup = '' 
 
-				for (let j = 0; j < value % 5; j++) {
+				for (let j = 0; j < value % nBatonnets; j++) {
 					lastGroup += c
 				}
 				
